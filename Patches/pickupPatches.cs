2a48a1160421ddadcdc89cbfda940d5548e965ec
YourTaxes <finnegan.mcdevitt@gmail.com>
2026-07-24 01:02:08 -0500

    Roulette item is now "grabbable" but causes errors when you do. it also highlights when you look at it, but it lost all color

--- Patches/pickupPatches.cs
+++ Patches/pickupPatches.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using DG.Tweening;
 using HarmonyLib;
 using UnityEngine;
 
@@ -14,16 +15,23 @@ public class FirstPersonControllerAwakePatch
 
         Vector3 spawnPos = __instance.transform.position + __instance.transform.forward * 5f;
         GameObject spawned = Object.Instantiate(Plugin.RouletteItemPrefab, spawnPos, Quaternion.identity);
-        spawned.AddComponent<ItemBehaviour>();
+        Weapon weaponComponent = spawned.GetComponent<WeaponHandSpawner>();
+        ItemBehaviour itemBehaviour = spawned.AddComponent<ItemBehaviour>();
+        itemBehaviour.weaponName = "Roulette Item";
+
         spawned.AddComponent<BoxCollider>();
         spawned.AddComponent<Rigidbody>();
 
-        Shader standardShader = Shader.Find("Standard");
+        Shader weaponShader = Shader.Find("S_WeaponOutline_00");
         foreach (Renderer renderer in spawned.GetComponentsInChildren<Renderer>())
         {
             foreach (Material mat in renderer.materials)
             {
-                mat.shader = standardShader;
+                Color savedColor = mat.HasProperty("_BaseColor") ? mat.GetColor("_BaseColor")
+                                 : mat.HasProperty("_Color")     ? mat.GetColor("_Color")
+                                 : Color.white;
+                mat.shader = weaponShader;
+                if (mat.HasProperty("_Color")) mat.SetColor("_Color", savedColor);
             }
         }
     }
@@ -39,6 +47,130 @@ public class GrabPatches
         //this script shows how weapons are set in a person's hand.
     }
 }
+
+[HarmonyPatch(typeof(PlayerPickup), "RpcLogic___DropObjectObserver_2127535046")]
+public class DropObserverPatch
+{
+    static bool Prefix(PlayerPickup __instance, GameObject obj, bool rightHand)
+    {
+        ItemBehaviour ib = obj?.GetComponent<ItemBehaviour>();
+        if (ib == null || ib.weaponName != "Roulette Item") return true;
+
+        Traverse t = Traverse.Create(__instance);
+
+        if (!__instance.IsOwner)
+            ib.StickOnGroundObservers();
+
+        obj.transform.DOKill(false);
+
+        if (__instance.IsOwner)
+        {
+            PauseManager.Instance.MoveAmmoDisplay(false, rightHand);
+            t.Field("playerController").GetValue<FirstPersonController>().isScopeAiming = false;
+            if (rightHand)
+            {
+                t.Field("weaponInHand").SetValue(null);
+                t.Field("behaviourInHand").SetValue(null);
+            }
+            else
+            {
+                t.Field("weaponInLeftHand").SetValue(null);
+                t.Field("behaviourInLeftHand").SetValue(null);
+            }
+        }
+
+        if (ib.rightHandAnim != "")
+        {
+            Animator anim = t.Field("animator").GetValue<Animator>();
+            anim.SetBool(rightHand ? ib.rightHandAnim : ib.leftHandAnim, false);
+        }
+
+        object camAnimScript = t.Field("camAnimScript").GetValue();
+        Traverse.Create(camAnimScript).Field("rotateBack").SetValue(true);
+
+        ib.playerPickup = null;
+        ib.playerController = null;
+        ib.rootObject = null;
+        ib.OnDrop(t.Field("cam").GetValue<Camera>());
+        // skipped: obj.GetComponent<Weapon>().camAnimScript = null — no Weapon on roulette item
+        ib.cam = null;
+        obj.transform.parent = null;
+        obj.transform.localScale = new Vector3(2f, 2f, 2f);
+        ib.UnsetLayer();
+        obj.layer = 7;
+        object rigBuilder = t.Field("RigBuilder").GetValue();
+        Traverse.Create(rigBuilder).Method("Build").GetValue();
+
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(ItemBehaviour), "OnDrop")]
+public class OnDropPatch
+{
+    static bool Prefix(ItemBehaviour __instance, Camera tempCam)
+    {
+        if (__instance.weaponName != "Roulette Item") return true;
+
+        Traverse t = Traverse.Create(__instance);
+        t.Field("dispenserStart").SetValue(false);
+        Rigidbody rb = __instance.GetComponent<Rigidbody>() ?? __instance.gameObject.AddComponent<Rigidbody>();
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+        rb.drag = 0f;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        float ejectForce = t.Field<float>("ejectForce").Value;
+        float torqueForce = t.Field<float>("torqueForce").Value;
+        rb.AddForce(tempCam.transform.forward * ejectForce, ForceMode.Impulse);
+        rb.AddTorque(tempCam.transform.forward * torqueForce + __instance.transform.right * torqueForce, ForceMode.Impulse);
+
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(ItemBehaviour), "Start")]
+public class StartPatches
+{
+    static void Prefix(ItemBehaviour __instance)
+    {
+        if (__instance.weaponName != "Roulette Item") return;
+
+        Transform gripRight = null, gripLeft = null;
+        foreach (Transform t in __instance.GetComponentsInChildren<Transform>())
+        {
+            if (t.name == "Grip_Right") gripRight = t;
+            else if (t.name == "Grip_Left") gripLeft = t;
+        }
+        Plugin.BepinLogger.LogInfo($"Grip_Right: {gripRight}, Grip_Left: {gripLeft}");
+
+        gripRight?.gameObject.AddComponent<Grip>();
+        gripLeft?.gameObject.AddComponent<Grip>();
+        Traverse.Create(__instance).Field("dispenserStart").SetValue(true);
+    }
+
+    static void Postfix(ItemBehaviour __instance)
+    {
+        Plugin.BepinLogger.LogInfo("Start called on weapon " + __instance.weaponName);
+        if (__instance.weaponName == "Roulette Item")
+        {
+            Plugin.BepinLogger.LogInfo("Roulette Item Start called");
+            var allMaterials = new System.Collections.Generic.List<Material>();
+            int counter = 0;
+            Plugin.BepinLogger.LogInfo($"Renderer length is {__instance.GetComponentsInChildren<Renderer>().Length}");
+            foreach (Renderer renderer in __instance.GetComponentsInChildren<Renderer>())
+            {
+                foreach (Material mat in renderer.materials)
+                {
+                    counter++;
+                    Plugin.BepinLogger.LogInfo($"Material {counter}: {mat.name}, Renderer: {renderer.name}, Shader: {mat.shader.name}");
+                    allMaterials.Add(mat);
+                }
+            }
+            Traverse.Create(__instance).Field("hoveredObjectMat").SetValue(allMaterials);
+            Plugin.BepinLogger.LogInfo("Roulette Item materials set");
+        }        
+    }
+}
+
 
 [HarmonyPatch(typeof(ItemBehaviour), "KillAnimation")]
 public class KillAnimationPatches
@@ -70,9 +202,9 @@ public class ClaymoreKillPatch
 [HarmonyPatch(typeof(Shotgun), "KillServer")]
 public class ShotgunKillPatch
 {
-    static void Postfix(ItemBehaviour ___behavior)
+    static void Postfix(ItemBehaviour __instance)
     {
-        Plugin.BepinLogger.LogInfo($"Player killed with {___behavior?.weaponName}");
+        Plugin.BepinLogger.LogInfo($"Player killed with {__instance?.weaponName}");
     }
 }
 
